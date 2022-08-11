b2d9f30a91822bdd8d99c4ba5be0548ad03386b7
Marcos <exegeses@gmail.com>
2022-08-11 10:09:46 -0300

    Notas setup Breeze

---
+++ breeze/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/marcas', function ()
+{
+    return view('marcas');
+})->middleware(['auth'])->name('marcas');
+
+Route::get('/uri', [ controller::class, 'method' ])
+    ->middleware(['auth'])
+    ->name('nombre');
+
+
+require __DIR__.'/auth.php';
